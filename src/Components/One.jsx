e60224c0ca023742164edf6602f5ee46a6134bc8
saalmansourav6122 <saalmansourav111@gmail.com>
2025-03-01 11:50:08 +0600

    component added

--- src/Components/One.jsx
+++ src/Components/One.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { motion } from "motion/react";
+import { div } from "motion/react-client";
 
 function One() {
-  return <div>One</div>;
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <motion.div
+          animate={{
+            scale: 2,
+            transition: { duration: 2 },
+          }}
+          className="box_one"
+        ></motion.div>
+      </div>
+    </div>
+  );
 }
 
 export default One;
